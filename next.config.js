7c11fc31e3b1a3eb3d4b3da425f24be5022ac4f8
d24479385-art <d24479385@gmail.com>
2026-02-05 00:00:04 +0530

    Add Next.js configuration file with strict mode

---
+++ next.config.js
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // Also ignore typescript errors during build
+    ignoreBuildErrors: true,
+  },
+}
+
+module.exports = nextConfig
